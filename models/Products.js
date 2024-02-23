c3256a1e889bc353ea2ec6c8eb759810af9dece3
Abdo Reda <gethup92@gmail.com>
2024-02-23 22:25:32 +0200

    add gitignore file

--- models/Products.js
+++ models/Products.js
@@ -5,14 +5,14 @@ const ProductSchema = new mongoose.Schema(
     title: { type: String, required: true, unique: true },
     desc: { type: String, required: true },
     img: { type: String, required: true },
-    //  categories: { type: Array },
-    //size: { type: Array },
-    // color: { type: Array },
+    categories: { type: Array, required: true },
+    size: { type: Array, required: true },
+    color: { type: Array, required: true },
     price: { type: Number, required: true },
     inStock: { type: Boolean, default: true },
-  },
-  { timestamps: true }
+  }
+  //{ timestamps: true }
 );
 
-const Product = mongoose.model("Product", ProductSchema);
-module.exports = Product;
+const Products = mongoose.model("Products", ProductSchema);
+module.exports = Products;
